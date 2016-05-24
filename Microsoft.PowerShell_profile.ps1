aa10053d484bf3a3ff5bebdd81dde5ff35fb39cc
Scott Banwart <sbanwart@stonedonut.com>
2016-05-24 10:24:56 -0400

    Replace VsVars scripts with Posh-VsVars module
    - Remove the scripts in the profile that set the Visual Studio environment variables
    - Add Posh-VsVars to the module list

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,29 +29,15 @@ function global:prompt {
 }
 
 # Load Visual Studio environment variables
-function Get-Batchfile ($file) {
-    $cmd = "`"$file`" & set"
-    cmd /c $cmd | Foreach-Object {
-        $p, $v = $_.split('=')
-        Set-Item -path env:$p -value $v
-    }
-}
+Import-Module Posh-VsVars
 
-function VsVars32($version = "12.0")
-{
-    #$key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $version
-    #$VsKey = get-ItemProperty $key
-    #$VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
-    #$VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
-    #$VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
-    $VsToolsDir = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools"
-    $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
-    Get-Batchfile $BatchFile
-    [System.Console]::Title = "Visual Studio " + $version + " Windows Powershell"
-    #add a call to set-consoleicon as seen below...hm...!
-}
-
-VsVars32
+Set-VsVars -Version 'latest'
 
 # Load Chocolatey profile
 Import-Module -Name C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
