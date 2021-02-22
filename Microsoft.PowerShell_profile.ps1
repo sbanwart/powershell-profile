83a4f46251a39eeb7b3b61d829804681bf539c38
Scott Banwart <sbanwart@acm.org>
2021-02-22 18:50:17 -0500

    Add Scoop completions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -22,32 +22,32 @@ Import-Module z
 # Alias to jump to my working directory
 function w { Set-Location C:\Work }
 
+# Load posh-git module
+Import-Module posh-git
+
+# Set git prompt
+#function global:prompt {
+#    $realLASTEXITCODE = $LASTEXITCODE
+#
+#    Write-Host($pwd.ProviderPath) -nonewline
+#
+#    Write-VcsStatus
+#
+#    $global:LASTEXITCODE = $realLASTEXITCODE
+#    return "> "
+#}
+
 # Load Starship prompt
 Invoke-Expression (&starship init powershell)
 
-# Load posh-git module
-#Import-Module posh-git
+# Load oh-my-posh module
+#Import-Module oh-my-posh
 
 # Configure oh-my-posh theme
 #Set-Theme Sorin
 
-# Set git prompt
-function global:prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
-
-    Write-Host($pwd.ProviderPath) -nonewline
-
-    Write-VcsStatus
-
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    return "> "
-}
-
-# Load oh-my-posh module
-Import-Module oh-my-posh
-
-# Configure oh-my-posh theme
-Set-Theme Sorin
+# Load Scoop completions
+Import-Module "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
 
 # Load Chocolatey profile
 Import-Module -Name C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
@@ -85,6 +85,5 @@ Set-Alias vsc VsCode-CurrDir
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-(& C:\Users\Scott\Anaconda3\Scripts\conda.exe "shell.powershell" "hook") | Out-String | Invoke-Expression
+# (& C:\Users\Scott\Anaconda3\Scripts\conda.exe "shell.powershell" "hook") | Out-String | Invoke-Expression
 #endregion
-
